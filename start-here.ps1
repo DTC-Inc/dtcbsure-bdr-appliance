b5d42a881f8dd518fbcdb9947854a1e5856fc654
Nate Smith <40123869+nettts@users.noreply.github.com>
2020-10-27 15:23:05 -0400

    Update start-here.ps1

--- start-here.ps1
+++ start-here.ps1
@@ -1,5 +1,5 @@
-Start-Process powershell -verb runAs -argumentList "-file $psScriptRoot\s2d-deploy.ps1" -Wait
-Start-Process powershell -verb runAs -argumentList "-file $psScriptRoot\create-share.ps1" -Wait
-Start-Process powershell -verb runAs -argumentList "-file $psScriptRoot\deploy-networking.ps1" -wait
-Start-Process powershell -verb runAs -argumentList "-file $psScriptRoot\dld-bob.boot.iso.ps1" -wait
-Start-Process powershell -verb runAs -argumentList "-file $psScriptRoot\agent-install.ps1" -wait
+& "$psScriptRoot\s2d-deploy.ps1"
+& "$psScriptRoot\create-share.ps1"
+& "$psScriptRoot\deploy-networking.ps1"
+& "$psScriptRoot\dld-bob.boot.iso.ps1"
+& "$psScriptRoot\agent-install.ps1"
