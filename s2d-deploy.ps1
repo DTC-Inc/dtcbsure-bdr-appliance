80845cb471739e735d807602eb3006b85004f96a
Nate Smith <40123869+nettts@users.noreply.github.com>
2020-10-27 16:47:21 -0400

    Fixed typo
    isReadyOnly to isReadOnly

--- s2d-deploy.ps1
+++ s2d-deploy.ps1
@@ -1,15 +1,15 @@
 param([switch]$elevated)
 Get-Disk | Where-Object isOffline -eq $true | Set-Disk -isOffline $false
 Update-StorageProviderCache
-Get-StoragePool | ? isPrimordial -eq $false | Set-StoragePool -isReadyOnly:$false -errorAction silentlyContinue
+Get-StoragePool | ? isPrimordial -eq $false | Set-StoragePool -isReadOnly:$false -errorAction silentlyContinue
 Get-StoragePool | ? isPrimorial -eq $false | Get-VirtualDisk | Remove-VirtualDisk -confirm:$false -errorAction silentlyContinue
 Get-StoragePool | ? isPrimordial -eq $false | Remove-StoragePool -confirm:$false -errorAction silentlyContinue
 Get-PhysicalDisk | Reset-PhysicalDisk -errorAction silentlyContinue
 Get-Disk | ? number -ne $null | ? isBoot -ne $true | ? isSystem -ne $true | ? partitionStyle -ne RAW | % {
     $_ | Set-Disk -isOffline:$false
-    $_ | Set-Disk -isReadyOnly:$false
+    $_ | Set-Disk -isReadOnly:$false
     $_ | Clear-Disk -removeData -removeOEM -confirm:$false
-    $_ | Set-Disk -isReadyOnly:$true
+    $_ | Set-Disk -isReadOnly:$true
     $_ | Set-Disk -isOffline:$true
 }
 Get-Disk | Where number -Ne $Null | Where isBoot -Ne $true | Where isSystem -Ne $true | Where partitionStyle -eq RAW | Group -noElement -property friendlyName
