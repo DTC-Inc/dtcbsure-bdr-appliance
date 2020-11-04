6785a35d7f16f40dd91f965f78a8d5502d5ce0a5
Nate Smith <40123869+nettts@users.noreply.github.com>
2020-11-04 10:05:09 -0500

    update

--- agent-install.ps1
+++ agent-install.ps1
@@ -1,6 +1,3 @@
 wget "https://automate.dtctoday.com/LabTech/Deployment.aspx?InstallerToken=803804a6951441c88b8ad92968a7f3c8" -outFile $env:windir\temp\agent_install.exe
 $agentInstallPath = "$env:windir\temp\agent_install.exe"
 Start-Process -filePath $agentInstallPath -argumentList "/quiet /install"
-$newName = Read-Host -Prompt "Input the DTCBSURE Appliance Name (DTCBSURE-$SERVICETAG): "
-Rename-Computer -NewName $newName
-
