1c1f4f84c03e938bdc33aca90481270bffd862bc
Nate Smith <40123869+nettts@users.noreply.github.com>
2020-11-04 10:20:46 -0500

    Update start-here.ps1

--- start-here.ps1
+++ start-here.ps1
@@ -8,6 +8,7 @@ if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 }
 
 Set-ExecutionPolicy -executionPolicy unrestricted -force -scope localMachine
+Remove-Item -path $env:windir\temp\dtcbsure-bdr.zip -force -confirm:$false
 wget "https://codeload.github.com/DTC-Inc/dtcbsure-bdr-appliance/zip/main" -outFile $env:windir\temp\dtcbsure-bdr.zip
 Expand-Archive -path "$env:windir\temp\dtcbsure-bdr.zip" -destinationPath "$env:systemdrive\dtc" -force
 
