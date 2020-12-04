af79758f31986c0eea8a12bdc50aa70f2e790f55
Nate Smith <40123869+nettts@users.noreply.github.com>
2020-12-04 11:42:41 -0500

    Update deploy-networking.ps1

--- deploy-networking.ps1
+++ deploy-networking.ps1
@@ -1,8 +1,6 @@
 Get-VMNetworkadapter -managementOS  | Where-Object -property "name" -notlike "Container NIC*" | Remove-VMNetworkAdapter
 Get-VMSwitch | Where-Object -property name -notlike "Default Switch" | Remove-VMSwitch -force
 Get-NetSwitchteam| Remove-NetSwitchTeam
-$newName = Read-Host -Prompt "Input the DTCBSURE Appliance Name (DTCBSURE-$SERVICETAG): "
-Rename-Computer -NewName $newName
 $toTeam = Get-NetAdapter | Where-Object -property interfaceDescription -like "Intel*" | Select-Object -expandProperty name
 New-NetSwitchTeam -name TEAM0 -teamMembers $toTeam
 New-VMSwitch -name TEAM0 -netAdapterName TEAM0
