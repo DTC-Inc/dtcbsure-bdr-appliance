9d5995a2e5544f91e11be72535db277a4c878c18
Nate Smith <40123869+nettts@users.noreply.github.com>
2020-12-04 14:30:50 -0500

    Update agent-install.ps1

--- agent-install.ps1
+++ agent-install.ps1
@@ -1,3 +1,3 @@
-wget "https://s3.us-west-002.backblazeb2.com/public-dtc/repo/dtcbsure-bdr/agent_install.msi" -outFile $env:windir\temp\agent_install.exe
+wget "https://s3.us-west-002.backblazeb2.com/public-dtc/repo/dtcbsure-bdr/agent_install.exe" -outFile $env:windir\temp\agent_install.exe
 $agentInstallPath = "$env:windir\temp\agent_install.exe"
 Start-Process -filePath $agentInstallPath -argumentList "/quiet /install"
