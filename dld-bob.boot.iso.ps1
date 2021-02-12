9a87a978fd3335ee763c884e6723a016a2305a0a
Nate Smith <40123869+nettts@users.noreply.github.com>
2021-02-12 16:18:27 -0500

    Update dld-bob.boot.iso.ps1

--- dld-bob.boot.iso.ps1
+++ dld-bob.boot.iso.ps1
@@ -1,1 +1,1 @@
-wget https://s3.us-west-002.backblazeb2.com/public-dtc/repo/images/winpe/bob.boot-current.ISO
+wget https://s3.us-west-002.backblazeb2.com/public-dtc/repo/images/winpe/bob.boot-current.ISO -OutFile D:\repo\bob.boot-current.ISO
